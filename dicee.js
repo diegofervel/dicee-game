967a3954c33e4c701fc6876109f20aece7fa371f
Diego Velasco <54513311+diegofervel@users.noreply.github.com>
2023-11-29 18:24:48 -0500

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,20 @@
+
+
+
+let imageNumber1 = Math.floor(Math.random()*6+1);
+
+let imageNumber2 = Math.floor(Math.random()*6+1);
+
+document.querySelector(".img1").setAttribute("src", "images/dice"+imageNumber1+".png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice"+imageNumber2+".png");
+
+if (imageNumber1 > imageNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+}
+else if (imageNumber2 > imageNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
